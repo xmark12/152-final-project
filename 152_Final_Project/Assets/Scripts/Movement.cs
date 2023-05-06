782a63007ee201450f962177d58c0f4b5f7ed10d
Kevin Ly <kevinly0128@gmail.com>
2023-05-05 20:09:36 -0700

    Player can now jump using space or w key

--- 152_Final_Project/Assets/Scripts/Movement.cs
+++ 152_Final_Project/Assets/Scripts/Movement.cs
@@ -10,17 +10,68 @@ public class Movement : MonoBehaviour
 
     public Animator a;
 
+    private Rigidbody2D rb;
+    public float speed = 7;
+    public float jumpForce = 13;
+    public float moveInput;
+
+    public Transform feetPos;
+    public float checkRadius = 0.3;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime = 0.15;
+
     // Start is called before the first frame update
     void Start()
     {
         grounded = true;
         jump = false;
         duck = false;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (grounded && (Input.GetKeyDown("space") || Input.GetKeyDown("w")))
+        {
+            jump = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+            
+        } else if (Input.GetKeyDown("s"))
+        {
+            print("duck key was pressed");
+        }
+
+        if ((Input.GetKey("space") || Input.GetKey("w")) && jump)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else
+            {
+                jump = false;
+            }
+        }
+
+        if (Input.GetKeyUp("space") || Input.GetKeyUp("w"))
+        {
+            jump = false;
+        }
+
+
         if (jump && !grounded && !duck)
         {
             a.Play("Jump");
