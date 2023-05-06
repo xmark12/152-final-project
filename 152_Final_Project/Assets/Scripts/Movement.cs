23a37488ea6a87634842578e67178d3f7bdb0f73
Kevin Ly <kevinly0128@gmail.com>
2023-05-05 22:59:15 -0700

    Player can now duck; jumping animation is also fixed

--- 152_Final_Project/Assets/Scripts/Movement.cs
+++ 152_Final_Project/Assets/Scripts/Movement.cs
@@ -7,20 +7,25 @@ public class Movement : MonoBehaviour
     public bool grounded;
     public bool jump;
     public bool duck;
+    public bool isJumping;
 
     public Animator a;
 
     private Rigidbody2D rb;
-    public float speed = 7;
-    public float jumpForce = 13;
-    public float moveInput;
+    public float jumpForce = 10;
 
     public Transform feetPos;
     public float checkRadius = 0.3F;
     public LayerMask whatIsGround;
 
     private float jumpTimeCounter;
-    public float jumpTime = 0.15F;
+    public float jumpTime = 0.35F;
+
+    public BoxCollider2D collider;
+    public Vector2 regularSize;
+    public Vector2 duckingSize;
+    public Vector2 regularOffset;
+    public float duckingOffsetY;
 
     // Start is called before the first frame update
     void Start()
@@ -28,47 +33,62 @@ public class Movement : MonoBehaviour
         grounded = true;
         jump = false;
         duck = false;
+        isJumping = false;
 
         rb = GetComponent<Rigidbody2D>();
-    }
+        collider = GetComponent<BoxCollider2D>();
 
-    void FixedUpdate()
-    {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        regularSize = collider.size;
+        duckingSize = collider.size;
+        duckingSize.y = duckingSize.y / 2;
+
+        regularOffset = collider.offset;
+        duckingOffsetY = -duckingSize.y / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
         grounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        jump = !grounded;
 
         if (grounded && (Input.GetKeyDown("space") || Input.GetKeyDown("w")))
         {
-            jump = true;
+            isJumping = true;
+            duck = false;
+            collider.size = regularSize;
+            collider.offset = regularOffset;
             jumpTimeCounter = jumpTime;
             rb.velocity = Vector2.up * jumpForce;
             
-        } else if (Input.GetKeyDown("s"))
+        } else if (grounded && Input.GetKeyDown("s"))
         {
-            print("duck key was pressed");
+            duck = true;
+            collider.size = duckingSize;
+            collider.offset = new Vector2(regularOffset.x, duckingOffsetY);
         }
 
-        if ((Input.GetKey("space") || Input.GetKey("w")) && jump)
+        if ((Input.GetKey("space") || Input.GetKey("w")) && isJumping)
         {
             if (jumpTimeCounter > 0)
             {
                 rb.velocity = Vector2.up * jumpForce;
+                
                 jumpTimeCounter -= Time.deltaTime;
             } else
             {
-                jump = false;
+                isJumping = false;
             }
         }
 
         if (Input.GetKeyUp("space") || Input.GetKeyUp("w"))
         {
-            jump = false;
+            isJumping = false;
+        } else if (Input.GetKeyUp("s"))
+        {
+            duck = false;
+            collider.size = regularSize;
+            collider.offset = regularOffset;
         }
 
 
